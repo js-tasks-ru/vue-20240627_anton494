f5b0e8bc71c880ac02094f05e7aa89578c292eba
Anton Yurovski <anton3.ru@mail.ru>
2024-07-16 19:08:39 +0300

    01-basics/10-create-app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,15 @@
 import { defineComponent, createApp } from 'vue/dist/vue.esm-bundler.js'
+import { useNavigatorLanguage } from '@vueuse/core'
+const App =  defineComponent({
+  name: 'App',
+  setup () {
+    const { language } = useNavigatorLanguage()
+    return {
+      language
+    }
+  },
+  template: `<div>Сегодня {{new Date().toLocaleDateString(language, { dateStyle: 'long' })}}</div>`,
+})
+
+const app = createApp(App)
+app.mount('#app')
